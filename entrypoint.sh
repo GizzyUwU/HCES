faf3ccedd1c90b7e1ac233fb5f89fea1f0852d7f
gizzy <nerop455@gmail.com>
2026-08-31 01:53:32 +0100

    swap it to gosu

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 export GIT_COMMIT_SHA="$(git --git-dir=/usr/src/app/.git rev-parse HEAD 2>/dev/null || echo unknown)"
 if [ "$WORKER" = "true" ] && [ -n "$ORCHESTRATOR_URL" ]; then
-    exec su-exec bun bun run prod:worker
+    exec gosu bun bun run prod:worker
 elif [ "$SERVICE" = "frontend" ]; then
-    exec su-exec bun bun run prod:frontend
+    exec gosu bun bun run prod:frontend
 else
-    exec su-exec bun bun run prod:backend
+    exec gosu bun bun run prod:backend
 fi
