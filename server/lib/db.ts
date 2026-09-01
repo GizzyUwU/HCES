968fb665b8f40e4efe55c7994693973e0107cc1d
gizzy <nerop455@gmail.com>
2026-09-01 13:46:26 +0100

    fix: it kept using db on remote workers

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -3,29 +3,40 @@ import { Pool } from "pg";
 import { readdir } from "node:fs/promises";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
-export let db: ReturnType<typeof drizzle> = drizzle({
-  client: new Pool({
-    connectionString: process.env.DATABASE_URL!,
-    max: Number(process.env["DB_POOL_MAX"]) || 20,
-    idleTimeoutMillis: 30 * 1000,
-    connectionTimeoutMillis: 5 * 1000,
-  }),
-});
-
-let migrationsExists = true;
-
-try {
-  await readdir("./migrations");
-} catch (error) {
-  if ((error as NodeJS.ErrnoException).code === "ENOENT") {
-    migrationsExists = false;
-  } else {
-    throw error;
-  }
-}
-
-if (migrationsExists) {
-  await migrate(db, {
-    migrationsFolder: "./migrations",
+export let db: ReturnType<typeof drizzle>;
+if (Boolean(process.env["WORKER"] && process.env["ORCHESTRATOR_URL"])) {
+  db = new Proxy({} as ReturnType<typeof drizzle>, {
+    get() {
+      throw new Error(
+        "db accessed in remote worker mode (WORKER + ORCHESTRATOR_URL). This code path should not run on a worker.",
+      );
+    },
   });
+} else {
+  db = drizzle({
+    client: new Pool({
+      connectionString: process.env.DATABASE_URL!,
+      max: Number(process.env["DB_POOL_MAX"]) || 20,
+      idleTimeoutMillis: 30 * 1000,
+      connectionTimeoutMillis: 5 * 1000,
+    }),
+  });
+
+  let migrationsExists = true;
+
+  try {
+    await readdir("./migrations");
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === "ENOENT") {
+      migrationsExists = false;
+    } else {
+      throw error;
+    }
+  }
+
+  if (migrationsExists) {
+    await migrate(db, {
+      migrationsFolder: "./migrations",
+    });
+  }
 }
