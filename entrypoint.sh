7f5fd0990e1daf9b52275600fbf57da67d65cc47
gizzy <nerop455@gmail.com>
2026-09-01 13:21:09 +0100

    fix: remove entrypoint prod:worker js use prod worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -e
 export GIT_COMMIT_SHA="$(git --git-dir=/usr/src/app/.git rev-parse HEAD 2>/dev/null || echo unknown)"
-if [ "$WORKER" = "true" ] && [ -n "$ORCHESTRATOR_URL" ]; then
-    exec su-exec bun bun run prod:worker
-else
-    exec su-exec bun bun run prod:backend
-fi
+su-exec bun bun run prod:backend
